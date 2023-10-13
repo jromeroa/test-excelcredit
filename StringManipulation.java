3f77ea51a7b54b177c14800f7d25ee6eefeebbce
Jhonny Romero <jromeroarenales@gmail.com>
2023-10-12 23:18:40 -0500

    Solution for the third problem

--- StringManipulation.java
+++ StringManipulation.java
@@ -1,7 +1,23 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class StringManipulation {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        do{
+            System.out.print("Enter the word(s) you want to know about in number of consonants and vowels: ");
+            String input = scanner.nextLine();
+            Map<String, Integer> result = countVowelsConsonants(input);
+            // Print the result 
+            System.out.println(result); 
+            System.out.print("Do you want to continue? (yes/no): ");
+        } while (scanner.nextLine().equalsIgnoreCase("yes"));
+        System.out.println("Goodbye!");
+        scanner.close();
+    }
 
     public static Map<String, Integer> countVowelsConsonants(String input) {
         Map<String, Integer> result = new HashMap<>();
@@ -28,6 +44,4 @@ public class StringManipulation {
 
         return result;
     }
-
-   
 }
